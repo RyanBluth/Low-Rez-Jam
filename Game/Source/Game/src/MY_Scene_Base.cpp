a0999d59b3066e09bcf2944c147d60c0f2cebbc0
seleb <sean.s.leblanc@gmail.com>
2016-04-05 16:10:29 -0400

    removed escape to exit
    the menu isn't a separate scene, so it just freezes

--- Game/Source/Game/src/MY_Scene_Base.cpp
+++ Game/Source/Game/src/MY_Scene_Base.cpp
@@ -68,13 +68,13 @@ void MY_Scene_Base::update(Step * _step){
 	controller->update(_step);
 
 	// basic debugging controls
-	if(keyboard->keyJustDown(GLFW_KEY_ESCAPE)){
+	/*if(keyboard->keyJustDown(GLFW_KEY_ESCAPE)){
 		// if the user hits escape on the menu, exit the game
 		// if the user hits escape anywhere else, take them to the menu
 		if(game->scenes["menu"] != this){
 			game->switchScene("menu", false);
 		}
-	}if(keyboard->keyJustDown(GLFW_KEY_F11)){
+	}*/if(keyboard->keyJustDown(GLFW_KEY_F11)){
 		game->toggleFullScreen();
 	}if(keyboard->keyJustDown(GLFW_KEY_1)){
 		cycleCamera();
